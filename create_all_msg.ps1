7e030f800c619f9f67cfce726263d74cbb5fa574
Liu Wenyuan <15816141883@163.com>
2021-05-23 07:53:01 -0500

    [skip ci] !!!

--- create_all_msg.ps1
+++ create_all_msg.ps1
@@ -1,25 +1,24 @@
-# thmsg.exe should in the same folder
+# thmsg.exe should in PATH
+# this should be run in project root
 
-# create msg from txt
-Get-ChildItem ..\dialogue_cn\ |
+# generate stage msgfiles
+Get-ChildItem .\dialogue_cn\ |
 Where-Object {$_.Name -match '^st.*gb18030' } |
 ForEach-Object{
     $parttern = 'st\d{2}\w'
     $match = select-string -Pattern $parttern -inputobject $_.BaseName
     $Base = $match.matches.groups[0].value
     $OutName =  $Base + '.msg'
-   .\thmsg.exe -c 18 $_.FullName ..\data\$OutName
+    thmsg -c 18 $_.FullName data\$OutName
 }
 
-# create ending from txt
-
-Get-ChildItem ..\dialogue_cn\ |
+# generate ending msgfiles
+Get-ChildItem .\dialogue_cn\ |
 Where-Object {$_.Name -match '^e.*gb18030' } |
 ForEach-Object{
     $parttern = '^e\d{2}'
     $match = select-string -Pattern $parttern -inputobject $_.BaseName
     $Base = $match.matches.groups[0].value
     $OutName =  $Base + '.msg'
-   .\thmsg.exe -e -c 18 $_.FullName ..\data\$OutName
+    thmsg -e -c 18 $_.FullName data\$OutName
 }
-
